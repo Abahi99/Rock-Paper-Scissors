4871792a2917489e6df59658406238118df29a17
Abahi99 <96925597+Abahi99@users.noreply.github.com>
2023-03-29 18:03:24 -0400

    Add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+Rock Paper Scissors Project
+
+
+JavaScript Project from The Odin Project. 
+
+
+Project Reflections:
+I created the logic behind this game. The user will play against a computer that has the ability to choose randomly Rock, Paper, or Scissor to compute with the user that has the ability to choose by just clicking in the wanted button. The winner should win five round first.
+
+It wasn't hard for me to create this simple game. However, I struggle a little bit on controlling the result element that I created to just show the winner and disappear.
+
--- main.js
+++ main.js
@@ -1,70 +1,95 @@
+let playerCount = 0;
+let computerCount = 0;
+const content = document.querySelector('#Sshow');
+const Round = document.querySelector("round")
+const message = document.querySelector('#Rmessage');
+
+
+const result = document.querySelector('.winner');
+const showResult = document.createElement('span');
+showResult.style.fontSize = "50px";
+showResult.style.color = "#3882F6";
+result.appendChild(showResult);
+
+
+
+function game(){
+
+const buttons = document.querySelectorAll('.button');
+    buttons.forEach((button) => {
+
+        button.addEventListener('click', () => {
+            const computerSelection = getComputerChoice();
+            let round = playRound(button.id, computerSelection);
+            message.textContent = `${round}`; 
+            if (round.includes("computer")){
+                computerCount++
+            }else if (round.includes("player")){
+                playerCount++
+            }
+            content.textContent = `${playerCount} - ${computerCount}`;
+            
+            if (playerCount === 5){
+                showResult.style.visibility = 'visible';
+                    showResult.textContent = `${"Player WON The Game"}`;
+                    playerCount = 0;
+                    computerCount = 0;
+            }else if (computerCount === 5){
+                    showResult.style.visibility = 'visible';
+                    showResult.textContent = `${"Computer WON The Game"}`;
+                    playerCount = 0;
+                    computerCount = 0;
+            } else{
+                showResult.style.visibility = 'hidden';
+            }
+
+                
+
+        
+        });
+    });
+}
+
+
 
 
 function getComputerChoice(){
-    const choice = ["Rock", "Paper", "Scissor"];
+  const choice = ["Rock", "Paper", "Scissor"];
 
-    let computer = Math.floor(Math.random()*choice.length);
+  let computer = Math.floor(Math.random()*choice.length);
 
-    let play = choice[computer];
+  let play = choice[computer];
 
-    return play
+  return play
 }
 
 function playRound(playerSelection, computerSelection) {
-        let player = playerSelection.toLowerCase();
-        let cs = computerSelection.toLowerCase();
+      let player = playerSelection.toLowerCase();
+      let cs = computerSelection.toLowerCase();
 
-        if (player === cs ) {
-            return "Tie!"
-        } 
-        else if (player === "rock" && cs === "paper"){
-            return `${cs} beats ${player}, computer wins`
-        }
-        else if (player === "rock" && cs === "scissor"){
-            return `${player} beats ${cs}, player wins`
-        }
+      if (player === cs ) {
+          return "Tie!"
+      } 
+      else if (player === "rock" && cs === "paper"){
+          return `${cs} beats ${player}, computer wins this round`
+      }
+      else if (player === "rock" && cs === "scissor"){
+          return `${player} beats ${cs}, player wins this round`
+      }
 
-        else if (player === "paper" && cs === "scissor"){
-            return `${cs} beats ${player}, computer wins`
-        }
-        else if (player === "paper" && cs === "rock"){
-            return `${player} beats ${cs}, player wins`
-        }
+      else if (player === "paper" && cs === "scissor"){
+          return `${cs} beats ${player}, computer wins this round`
+      }
+      else if (player === "paper" && cs === "rock"){
+          return `${player} beats ${cs}, player wins this round`
+      }
 
-        else if (player === "scissor" && cs === "rock"){
-            return `${cs} beats ${player}, computer wins`
-        }
-        else if (player === "scissor" && cs === "paper"){
-            return `${player} beats ${cs}, player wins`
-        }
-}
-
-function game(){
-    let playerCount = 0;
-    let computerCount = 0;
-
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt("Choose (Paper, Rock, or Scissor)");
-        const computerSelection = getComputerChoice();
-        let round = playRound(playerSelection, computerSelection);
-        console.log(round);
-
-        if (round.includes("computer")){
-            computerCount++
-        }else if (round.includes("player")){
-            playerCount++
-        }else{
-            continue
-        }
-    }
-
-    if (playerCount > computerCount){
-        console.log("Player WON")
-    }else if (computerCount > playerCount){
-        console.log("Computer WON")
-    }else{
-        console.log("TIE!!!")
-    }
+      else if (player === "scissor" && cs === "rock"){
+          return `${cs} beats ${player}, computer wins this round`
+      }
+      else if (player === "scissor" && cs === "paper"){
+          return `${player} beats ${cs}, player wins this round`
+      }
 }
 
 game()
